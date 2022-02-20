8f6f58710904956d837dc51f27af6a01e2a43a45
Rafael Dias de Almeida <94787506+rafaeldgeo@users.noreply.github.com>
2022-02-20 18:36:23 -0300

    em desenvolvimento

--- matriz-eletrica-br/dados/termonucleares.js
+++ matriz-eletrica-br/dados/termonucleares.js
@@ -1,10 +1,10 @@
-var nuclear = {
+var termonuclear = {
 "type": "FeatureCollection",
+"name": "termonucleares",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-                                                                                                                                  
 "features": [
-{ "type": "Feature", "properties": { "NOME": "Almirante Álvaro Alberto - Unidade III (Antiga Angra III)", "PROPRIETAR": "EletrobrásTermonuclear S\/A.", "UF": "RJ", "MUNIC": "Angra dos Reis", "COMBUSTIVE": "Urânio", "FASE_USINA": "Construção" }, "geometry": { "type": "Point", "coordinates": [ -44.469710611999972, -23.00907927499998 ] } },
-{ "type": "Feature", "properties": { "NOME": "Almirante Álvaro Alberto - Unidade I (Antiga Angra I)", "PROPRIETAR": "EletrobrásTermonuclear S\/A.", "UF": "RJ", "MUNIC": "Angra dos Reis", "COMBUSTIVE": "Urânio", "FASE_USINA": "Operação" }, "geometry": { "type": "Point", "coordinates": [ -44.457444443999975, -23.008711111999958 ] } },
-{ "type": "Feature", "properties": { "NOME": "Almirante Álvaro Alberto - Unidade II (Antiga Angra II)", "PROPRIETAR": "EletrobrásTermonuclear S\/A.", "UF": "RJ", "MUNIC": "Angra dos Reis", "COMBUSTIVE": "Urânio", "FASE_USINA": "Operação" }, "geometry": { "type": "Point", "coordinates": [ -44.458761110999944, -23.007208333999927 ] } }
+{ "type": "Feature", "properties": { "NOME": "Almirante Álvaro Alberto - Unidade III (Antiga Angra III)", "PROPRIETAR": "EletrobrásTermonuclear S/A.", "UF": "RJ", "MUNIC": "Angra dos Reis", "COMBUSTIVE": "Urânio", "FASE_USINA": "Construção", "MATRIZ": "Termonuclear" }, "geometry": { "type": "Point", "coordinates": [ -44.469710611999972, -23.00907927499998 ] } },
+{ "type": "Feature", "properties": { "NOME": "Almirante Álvaro Alberto - Unidade I (Antiga Angra I)", "PROPRIETAR": "EletrobrásTermonuclear S/A.", "UF": "RJ", "MUNIC": "Angra dos Reis", "COMBUSTIVE": "Urânio", "FASE_USINA": "Operação", "MATRIZ": "Termonuclear" }, "geometry": { "type": "Point", "coordinates": [ -44.457444443999975, -23.008711111999958 ] } },
+{ "type": "Feature", "properties": { "NOME": "Almirante Álvaro Alberto - Unidade II (Antiga Angra II)", "PROPRIETAR": "EletrobrásTermonuclear S/A.", "UF": "RJ", "MUNIC": "Angra dos Reis", "COMBUSTIVE": "Urânio", "FASE_USINA": "Operação", "MATRIZ": "Termonuclear" }, "geometry": { "type": "Point", "coordinates": [ -44.458761110999944, -23.007208333999927 ] } }
 ]
 }
